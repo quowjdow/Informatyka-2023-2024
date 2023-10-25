e7d2dc3e63e110f017f17714038098f142a94cb4
quowjdow <asdmiosahd189@wp.pl>
2023-10-25 15:04:27 +0200

    25.10.2023

---
+++ Lekcja 1/Untitled1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std; 
+ 
+ int main(){
+ 	
+ 	// deklaracja zmiennych
+ 	short liczba = 2;
+ 	float zmiennoprzecinkowa = 3.21;
+ 	string tekst = "tekst";
+ 	char znak = 'A';
+ 	bool logiczny = false;
+ 	/*
+ 	cout << "Hello World";
+ 	cout << endl;	// koniec lini
+ 	cout << "Liczba" << liczba;	// wyswietlanie zmienne
+ 	cout << endl;
+ 	
+ 	string wyraz;
+ 	cout << "Podaj jakis wyraz ";
+ 	cin >> wyraz;
+ 	cout << "Podales wyraz " << wyraz;
+ 	*/	
+ 	string imie;
+	 cout << "Podaj swoje imie ";
+ 	cin >> imie;
+ 	short wiek;
+	cout << "Podaj wiek ";
+ 	cin >> wiek;
+	cout << "witaj " << imie << "masz " << wiek << "lat ";
+	  
+	 
+	 return 0;
+ 	
+ }
