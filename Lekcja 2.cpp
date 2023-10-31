b66f692513f79007ec0b63876ef88b74ae4d825c
quowjdow <asdmiosahd189@wp.pl>
2023-10-31 15:00:24 +0100

    31.10.2023

---
+++ Lekcja 2.cpp
@@ -0,0 +1,36 @@
+	#include <iostream>
+	using namespace std;
+	
+	int main (){
+	// liczby calkowite - short, int, long, unsigned short
+	short a, b;
+	cout << "Podaj a: ";
+	cin >> a;
+	cout << "Podaj b: ";
+	cin >> b;
+	
+	// instrukcja warunkowa - if(warunek)
+	if(a > b){ // jezeli warunek sie spelni to wykona to co jest w klamrach
+		cout << a << " > " << b; // cout << zmienna << "tekst" << zmienna:
+	}
+	// = operator przypisania
+	// == operator porownania
+	// !=  Nie rowne (rozne od)
+	else if(a == b{ // jezeli zaden z powyzszych warunkow sie nie spelnil to wykonuje else
+		cout << a <<" < " << b;
+	}
+	
+cout << endl;
+system("pause");
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
