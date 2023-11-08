18a2186e0b7fd6d29fb27d531ba953c48d67a2a5
quowjdow <asdmiosahd189@wp.pl>
2023-11-08 15:00:58 +0100

    8.11.23

---
+++ Lekcja 3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+	for (int i=1; 1 <= 10; i++){
+		 for (int j = 1; <= 10; j++) {
+		 	std::cout << i << " * " << j << " = " << (i * j) << std::endl;
+		 {
+	}
+	
+	return 0;
+	
+}
+	
+
+
+
+
+
+
+
+
+
+
+}
